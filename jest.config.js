b2e4bf69a3862a7ba471b33b8f660977bf316bfb
gustavo <gustavool@live.com>
2023-03-16 15:02:31 -0300

    babel config removed

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   // collectCoverage: true,
@@ -18,3 +25,5 @@ module.exports = {
   //   "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   // },
 };
+
+module.exports = createJestConfig(customJestConfig);
